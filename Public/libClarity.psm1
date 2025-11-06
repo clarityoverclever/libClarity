dd83f6969674c317cbb8f2e97cd8fa9478fda2b8
Keith Marshall <clarityoverclever@protonmail.com>
2025-11-05 16:47:22 -0800

    refactor (loader): replaced if logic with switch case
    - removed multiple .ToLower() calls on functions by checking once
      and filtering the results through the function loader

--- Public/libClarity.psm1
+++ Public/libClarity.psm1
@@ -1,8 +1,9 @@
 # ---
 # Author:    Keith Marshall
 # Domain:    Public
-# Role:      Helper
-# Platform:  CrossPlatform
+# Role:      Module Loader
+# Platform:  CrossPlatform (Tested: Linux)
+# Edition:   Core
 # PSVersion: >=7.2
 # ---
 
@@ -18,30 +19,34 @@ try {
     [object] $groupedByDomain = (Get-FunctionMap).GetEnumerator() | Group-Object { $_.Value.Domain }
 
     foreach ($domainGroup in $groupedByDomain) {
-        # source private functions
-        if ($domainGroup.Name.ToLower() -eq 'private') {
-            foreach ($function in $domainGroup.Group) {
-                $name = $function.Key
-                $file = $function.Value.Path
+        switch -Regex ($domainGroup.Name.ToLower()) {
+        
+            'private' { # source private functions
+                foreach ($function in $domainGroup.Group) {
+                    $name = $function.Key
+                    $file = $function.Value.Path
 
-                try {
-                    . $file
-                } catch {
-                    Write-Error -Message "Failed to load $($name): $($_.Exception.Message)"
+                    try {
+                        . $file
+                        Write-Verbose "Loaded Private function: $name"
+                    } catch {
+                        Write-Error -Message "Failed to load $($name): $($_.Exception.Message)"
+                    }
                 }
             }
-        }
-        # export public functions
-        if ($domainGroup.Name.ToLower()-eq 'public') {
-            foreach ($function in $domainGroup.Group) {
-                $name = $function.Key
-                $file = $function.Value.Path
 
-                try {
-                    . $file
-                    Export-ModuleMember -Function $name
-                } catch {
-                    Write-Error -Message "Failed to load $($name): $($_.Exception.Message)"
+            'Public' { # export public functions
+                foreach ($function in $domainGroup.Group) {
+                    $name = $function.Key
+                    $file = $function.Value.Path
+
+                    try {
+                        . $file
+                        Export-ModuleMember -Function $name
+                        Write-Verbose "Exported Public function: $name"
+                    } catch {
+                        Write-Error -Message "Failed to load $($name): $($_.Exception.Message)"
+                    }
                 }
             }
         }
